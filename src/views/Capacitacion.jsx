effa8984f4d9a56b2405ae11b23b77ac2ba94d96
Cristian Rodriguez <cristiandosespigas@gmail.com>
2023-08-02 01:56:04 -0300

    creando menu burger

--- src/views/Capacitacion.jsx
+++ src/views/Capacitacion.jsx
@@ -1,10 +1,9 @@
-import video from "../assets/demo.mp4";
 const Capacitacion = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-white my-7 font-bold  text-[1.5rem] text-center md:text-[2rem]">Video demo de capacitación</h1>
       <video className="w-[1000px] " controls>
-        <source src={video} type="video/webm" />
+        <source src='https://res.cloudinary.com/dwy6oevco/video/upload/v1690951990/fqemhds6garkotsp8kws.webm' type="video/webm" />
         Your browser does not support the video tag.
       </video>
     </div>
